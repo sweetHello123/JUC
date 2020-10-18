25175c0b3742b51022467602eb97d775279bca15
china wu <18896820691@163.com>
2020-10-18 23:13:52 +0800

    update

--- src/main/java/com/china/concurrent/demo3/ExecutorsDemo.java
+++ src/main/java/com/china/concurrent/demo3/ExecutorsDemo.java
@@ -2,26 +2,76 @@ package com.china.concurrent.demo3;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * @Author: china wu
- * @Description: Executors工具类创建线程池
- * @Date: 2020/10/15 0:02
+ * @Description: Executors工具类创建线程池(实际工作不使用,并发下容易导致OOM)
+ * @Date: 2020/10/18 0:31
  */
 public class ExecutorsDemo {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        testFixedThreadPool();
+//        testSingleThreadPool();
+        testCacheThreadPool();
+//        testScheduledThreadPool();
+    }
+
+
+    private static void testFixedThreadPool() {
+        // 创建一个固定线程数量的线程池
+        ExecutorService service = Executors.newFixedThreadPool(5);
 
         try {
-            for (int i = 0; i < 5; i++) {
-                // 利用线程池创建线程
-                executorService.execute(() -> {
-                    System.out.println(Thread.currentThread().getName());
+            // 10个用户办理业务
+            for (int i = 1; i <= 10; i++) {
+                service.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "-办理");
                 });
             }
         } finally {
-            executorService.shutdown();
+            service.shutdown();
+        }
+    }
+
+    private static void testSingleThreadPool() {
+        // 创建一个单个线程的线程池
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        try{
+            for (int i = 1; i <= 5; i++) {
+                service.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "-ok");
+                });
+            }
+        }finally {
+            service.shutdown();
+        }
+    }
+
+    private static void testCacheThreadPool() {
+        // 创建一个N个线程的线程池
+        ExecutorService service = Executors.newCachedThreadPool();
+        try{
+            for (int i = 1; i <= 6; i++) {
+                service.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "-yes");
+                });
+            }
+        }finally {
+            service.shutdown();
+        }
+    }
+
+    private static void testScheduledThreadPool() {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
+        try{
+            for (int i = 1; i <= 6; i++) {
+                service.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "-s");
+                });
+            }
+        }finally {
+            service.shutdown();
         }
     }
 }
-
